b9ed57dbd341bec4abe7285d4731a8a8e092becd
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-09-25 22:21:19 -0400

    api initialized

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,53 @@ def get_contacts():
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({'contacts': json_contacts})
 
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+    
+    if not first_name or not last_name or not email:
+        return(jsonify({"MESSAGE": "All fields are required, include firstname, lastname and email"}), 400,)
+    
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created successfully"}), 201
+
+@app.route("/update_contact/<int:id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+    
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+    
+    db.session.commit()
+    
+    return jsonify({"message": "User updated successfully"}), 200
+
+@app.route("/delete_contact/<int:id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+    
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    db.session.delete(contact)
+    db.session.commit()
+    
+    return jsonify({"message": "User deleted successfully"}), 200
+    
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
