0853b7c65381c745f0c78732b2794d5dd19b1730
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-09-25 21:20:19 -0400

    main.py initialized

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-app,config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,10 @@
 from config import db
 
 class Contact(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=true, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     
     def to_json(self):
         return {
