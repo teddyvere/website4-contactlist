7bc756219acdd7342bfaa4c55801bed8bdb08c68
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-09-26 21:55:29 -0400

    contact form

---
+++ frontend--/src/ContactForm.jsx
@@ -0,0 +1,50 @@
+import {useState} from "react"
+
+const ContactForm = ({}) => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [Email, setEmail] = useState("")
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+        const url = "https://127.0.0.1.5000/create_contact"
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status!== 200) {
+            const message = await response.json()
+            alert(data.message)
+        } else {
+            // successfully created contact
+        }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="text" id="Email" value={Email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <button type="submit">Create Contact</button>
+        </form>
+)}}
+
+export default ContactForm
