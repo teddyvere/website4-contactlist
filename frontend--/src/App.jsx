da35a2330527293a82cef5b8da615a6219e02fd4
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-09-27 20:11:35 -0400

    modal created

--- frontend--/src/App.jsx
+++ frontend--/src/App.jsx
@@ -1,28 +1,47 @@
-import { useState, useEffect } from 'react'
-import ContactList from './ContactList'
-import ContactForm from './ContactForm'
-import './App.css'
+import { useState, useEffect } from 'react';
+import ContactList from './ContactList';
+import ContactForm from './ContactForm';
+import './App.css';
 
-  function App() {
-    const [contacts, setContacts] = useState([])
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-     fetchContacts()
-    }, [])
+  useEffect(() => {
+    fetchContacts();
+  }, []);
 
-    const fetchContacts = async () => {
-      const response = await fetch("http://127.0.0.1:5000/contacts")
-      const data = await response.json()
-      setContacts(data.contacts)
-      console.log(data.contacts)
-    }
-    
-    return (
-      <>
-        <ContactList contacts={contacts} />
-        <ContactForm />  {/* Render ContactForm */}
-      </>
-    );
-  }
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
 
-export default App
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <ContactList contacts={contacts} />
+      <button onClick={openCreateModal}>Create New Contact</button> {/* Fixed onClick typo and removed incorrect JSX */}
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
